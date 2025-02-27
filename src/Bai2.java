1d9e5a085952e1da793692bc46a65f93baf8c683
thecuongk5hp <theccuong2k5hp@gmail.com>
2025-02-27 17:03:02 +0700

    Session 01

---
+++ src/Bai2.java
@@ -0,0 +1,5 @@
+public class Bai2 {
+    public static void main(String[] args) {
+        System.out.println("Tên tôi là Nguyễn Thế Cường, tôi 20 tuổi, hiện tại đang học tại PTIT-HN. Quê ở Hải Phòng");
+    }
+}
